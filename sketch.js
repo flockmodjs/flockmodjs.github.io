c7395a435ce6c1ec1697769238e3957c25ecd543
lockness-Ko <42625905+lockness-Ko@users.noreply.github.com>
2019-08-14 18:37:19 +1000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -144,14 +144,17 @@ socket.on('draw_line', function(data) {
   strokeWeight(data[7]);
   line(data[0], data[1], data[2], data[3]);
   noStroke();
+  ls.push(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7]);
 });
 socket.on('draw_circle', function(data) {
   noStroke();
   fill(data[3], data[4], data[5]);
   circle(data[0], data[1], data[2]);
+  cs.push(data[0], data[1], data[2], data[3], data[4], data[5]);
 });
 socket.on('draw_rect', function(data) {
   noStroke();
   fill(data[3], data[4], data[5]);
   rect(data[0], data[1], data[2], data[2]);
+  ss.push(data[0], data[1], data[2], data[3], data[4], data[5]);
 });
